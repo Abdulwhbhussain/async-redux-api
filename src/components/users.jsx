9422b2dfe2907d896822a1229bd51db205a9ec67
WHB901 <abdulwhbhussain@gmail.com>
2023-06-14 10:57:47 +0500

    Followed till step 10

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUsers } from '../redux/users/usersSlice';
 
 function Users() {
+  const dispatch = useDispatch();
   const { users, isLoading, error } = useSelector((state) => state.users)
   console.log(users, isLoading, error);
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [dispatch])
 
   if (isLoading) return <div>Loading...</div>
 
--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -1,4 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const UsersUrl = "https://randomuser.me/api/?results=5";
+
+export const fetchUsers = createAsyncThunk(
+  "users/fetchUsers",
+  async (thunkAPI) => {
+    try {
+      const response = await fetch(UsersUrl);
+      if (!response.ok) {
+        throw new Error("Server Error!");
+      }
+      const users = await response.json();
+      return users.results;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.message });
+    }
+  }
+);
 
 const initialState = {
   users: [],
@@ -9,7 +27,19 @@ const initialState = {
 const usersSlice = createSlice({
   name: "users",
   initialState,
-  extraReducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.users = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload.error;
+    });
+  },
 });
 
 export default usersSlice.reducer;
